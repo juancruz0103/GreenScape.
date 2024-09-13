c15f6e9ad61789deb20bfac5cfbc1ce8ecab15b2
ZacariTest <zacaritest@gmail.com>
2024-09-13 12:46:18 -0300

    aa

--- GreenScape/src/Main.java
+++ GreenScape/src/Main.java
@@ -1,10 +1,8 @@
-import javax.swing.JOptionPane;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		JOptionPane.showMessageDialog(null, "Soy zacaraias");
 
 	}
 
