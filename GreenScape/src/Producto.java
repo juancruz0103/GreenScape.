829feea4285a49177a4d0468c69e7e4f5e870bd0
ZacariTest <157165983+ZacariTest@users.noreply.github.com>
2024-09-16 21:54:44 -0300

    Volvacado de clases V2

--- GreenScape/src/Producto.java
+++ GreenScape/src/Producto.java
@@ -1,4 +1,62 @@
 
 public class Producto {
+    private String nombre;
+    private int idProducto;
+    private String descripcion;
+    private double precio;
+    private int stock;
 
+    public Producto(String nombre, int idProducto, String descripcion, double precio, int stock) {
+        this.nombre = nombre;
+        this.idProducto = idProducto;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.stock = stock;
+    }
+
+    public void actualizarProducto(String descripcion, double precio, int stock) {
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.stock = stock;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(int idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
 }
--- GreenScape/src/Proyecto.java
+++ GreenScape/src/Proyecto.java
@@ -1,4 +1,58 @@
+import java.util.Date;
 
 public class Proyecto {
+    private String nombre;
+    private int idProyecto;
+    private Date fechaInicio;
+    private String estado;
 
+    public Proyecto(String nombre, int idProyecto, Date fechaInicio, String estado) {
+        this.nombre = nombre;
+        this.idProyecto = idProyecto;
+        this.fechaInicio = fechaInicio;
+        this.estado = estado;
+    }
+    
+    // Para añadir con la base de datos: Logica para iniciar un proyecto, para mostrar el estado del proyecto.
+
+    public void iniciarProyecto() {
+       
+    }
+
+    public void mostrarEstado() {
+        
+    }
+
+ 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getIdProyecto() {
+        return idProyecto;
+    }
+
+    public void setIdProyecto(int idProyecto) {
+        this.idProyecto = idProyecto;
+    }
+
+    public Date getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(Date fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
 }
