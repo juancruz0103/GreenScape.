44b201456783a30ac3b63d7edfe78a3b3b944229
ZacariTest <157165983+ZacariTest@users.noreply.github.com>
2024-09-19 18:54:42 -0300

    Creacion de un login para los administradores.

--- GreenScape/src/Main.java
+++ GreenScape/src/Main.java
@@ -1,9 +1,28 @@
+import javax.swing.JOptionPane;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        
+        List<Usuario> usuarios = new LinkedList<>();
+        usuarios.add(new Usuario("Admin", 1, "admin@gmail.com", "adminpass", "admin"));
+        usuarios.add(new Usuario("Jardinero", 2, "jardinero@gmail.com", "jardinpass", "jardinero"));
+        usuarios.add(new Usuario("Almacen", 3, "almacen@gmail.com", "almacenpass", "almacen"));
 
-	}
+        
+        String email = JOptionPane.showInputDialog("Ingrese su email:");
+        String contrasena = JOptionPane.showInputDialog("Ingrese su contraseña:");
 
+        
+        Usuario usuarioLogueado = Usuario.login(email, contrasena, usuarios);
+
+        if (usuarioLogueado != null) {
+            JOptionPane.showMessageDialog(null, "Login exitoso. Bienvenido " + usuarioLogueado.getNombre());
+
+        } else {
+            JOptionPane.showMessageDialog(null, "Credenciales incorrectas. Intentarlo de nuevo.");
+        }
+    }
 }
