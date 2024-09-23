994bc343e0fd9ef9459dd19c7a3f056663e40eaa
juancruz0103 <juancruzjcv0@gmail.com>
2024-09-23 18:41:05 -0300

    DEMO-GreenScape

---
+++ GreenScape/src/GestionProductos.java
@@ -0,0 +1,44 @@
+
+import javax.swing.JOptionPane;
+import java.util.LinkedList;
+
+public class GestionProductos {
+    private LinkedList<Producto> productos;
+
+    public GestionProductos() {
+        this.productos = new LinkedList<>();
+    }
+
+    public void agregarProducto(Producto producto) {
+        productos.add(producto);
+    }
+
+    public void mostrarProductos() {
+        if (productos.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "No hay productos disponibles", "Lista de Productos", JOptionPane.INFORMATION_MESSAGE);
+            return;
+        }
+
+        StringBuilder productosStr = new StringBuilder();
+        productosStr.append("ID\tNombre\tDescripción\tPrecio\tStock\n");
+
+        for (Producto producto : productos) {
+            productosStr.append(producto.getIdProducto())
+                         .append("\t")
+                         .append(producto.getNombre())
+                         .append("\t")
+                         .append(producto.getDescripcion())
+                         .append("\t")
+                         .append(producto.getPrecio())
+                         .append("\t")
+                         .append(producto.getStock())
+                         .append("\n");
+        }
+
+        JOptionPane.showMessageDialog(null, productosStr.toString(), "Lista de Productos", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    public LinkedList<Producto> getProductos() {
+        return productos;
+    }
+}
--- GreenScape/src/ListUsuario.java
+++ GreenScape/src/ListUsuario.java
@@ -21,6 +21,8 @@ public class ListUsuario {
 		usuarios.add(new  Usuario("Andres",0 ,"Andres@gmail.com","Andrespass", "Admin"));
 		usuarios.add(new  Usuario("Juan",0 ,"Juan@gmail.com","Juanpass", "Admin"));
 		usuarios.add(new  Usuario("Lucas",0 ,"Lucas@gmail.com","Lucasspass", "Admin"));
+		usuarios.add(new  Usuario("Pepe",0 ,"Pepe@gmail.com","Pepespass", "Usuario"));
+		
 
 	}
 }
